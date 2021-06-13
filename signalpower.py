035f172afe2aeaa9fc3a966c73deef7f35c03190
mohamedraslan85 <75303084+mohamedraslan85@users.noreply.github.com>
2021-06-13 22:35:28 +0200

    Update signalpower.py

--- signalpower.py
+++ signalpower.py
@@ -18,7 +18,52 @@ import datetime
 import numpy as np
 import TAcharts
 from tapy import Indicators
-import ta_py as ta;
+import ta_py as ta
+import requests
+import os
+import sys
+
+
+# check if the library folder already exists, to avoid building everytime you load the pahe
+if not os.path.isdir("/tmp/ta-lib"):
+
+    # Download ta-lib to disk
+    with open("/tmp/ta-lib-0.4.0-src.tar.gz", "wb") as file:
+        response = requests.get(
+            "http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
+        )
+        file.write(response.content)
+    # get our current dir, to configure it back again. Just house keeping
+    default_cwd = os.getcwd()
+    os.chdir("/tmp")
+    # untar
+    os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
+    os.chdir("/tmp/ta-lib")
+    os.system("ls -la /app/equity/")
+    # build
+    os.system("./configure --prefix=/home/appuser")
+    os.system("make")
+    # install
+    os.system("make install")
+    # bokeh sample data
+    os.system("bokeh sampledata")
+    # install python package
+    os.system(
+        'pip3 install --global-option=build_ext --global-option="-L/home/appuser/lib/" --global-option="-I/home/appuser/include/" ta-lib'
+    )
+    # back to the cwd
+    os.chdir(default_cwd)
+    print(os.getcwd())
+    sys.stdout.flush()
+
+# add the library to our current environment
+from ctypes import *
+
+lib = CDLL("/home/appuser/lib/libta_lib.so.0")
+# import library
+import talib
+
+
 hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
