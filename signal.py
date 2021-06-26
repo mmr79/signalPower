18e617fc7f1236fbd8cf24056e153cda89e5501b
mohamedraslan85 <75303084+mohamedraslan85@users.noreply.github.com>
2021-06-26 12:00:15 +0200

    Update signal.py

--- signal.py
+++ signal.py
@@ -79,18 +79,18 @@ def job1d():
     signal(symbols,['1d'],db)
 
 # Schedule a periodic task: do job every 60 seconds
-
-manager.register_task(name="task1", job=job15m).period(900).start_at("11:30:00").start()
-manager.register_task(name="task2", job=job1h).period(3600).start_at("11:00:00").start()
-manager.register_task(name="task3", job=job4h).period(14400).start_at("10:00:00").start()
-manager.register_task(name="task4", job=job1d).period_day_at("02:00:00").start()
+def update_db():
+    manager.register_task(name="task1", job=job15m).period(900).start_at("11:30:00").start()
+    manager.register_task(name="task2", job=job1h).period(3600).start_at("11:00:00").start()
+    manager.register_task(name="task3", job=job4h).period(14400).start_at("10:00:00").start()
+    manager.register_task(name="task4", job=job1d).period_day_at("02:00:00").start()
 
 @st.cache(allow_output_mutation=True)
 def Call_db():
     final,times=Call_db_signals()
     return final,times
 final,times =Call_db()
-
+update_db()
 flag=st.button('Update')
 if flag==1:
     caching.clear_cache()
